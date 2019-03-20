9091dadc915cdb1cfc24f7008eb5be9dbc023dc3
Thomas Beckett <tombeckett42@live.ca>
2019-03-19 22:58:50 -0500

    Initial unit test commit

---
+++ MyCharacterSheetTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyCharacterSheetTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyCharacterSheetTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("621d38ec-acb4-4487-a421-3ee5b0f9904e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyCharacterSheetTests/Skills Tests/SkillsTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyCharacterSheetTests.Skills_Tests
+{
+    [TestClass]
+    public class SkillsTests
+    {
+        [TestMethod]
+        public void Bonus_AcrobaticsSpecified_ShouldWork()
+        {
+            
+        }
+    }
+}
