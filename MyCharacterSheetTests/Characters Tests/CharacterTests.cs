9d5a2702a3b9243fb92fc4096ea679a6322ec38c
Thomas Beckett <tombeckett42@live.ca>
2019-03-23 17:17:19 -0500

    Updated character tests

--- MyCharacterSheetTests/Characters Tests/CharacterTests.cs
+++ MyCharacterSheetTests/Characters Tests/CharacterTests.cs
@@ -18,6 +18,56 @@ namespace MyCharacterSheetTests.Characters_Tests
         }
 
         [TestMethod]
+        public void IsDead_MinDamage_ShouldNotWork()
+        {
+            Character character = new Character();
+
+            character.LoadCharacterSheetFromString(GetTestXml());
+            character.HitPoints.HP = character.HitPoints.MaxHP;
+
+            Assert.IsFalse(character.IsDead(1));
+        }
+
+        [TestMethod]
+        public void IsDead_MaxDamage_ShouldWork()
+        {
+            Character character = new Character();
+
+            character.LoadCharacterSheetFromString(GetTestXml());
+            character.HitPoints.HP = 1;
+
+            Assert.IsTrue(character.IsDead(character.HitPoints.MaxHP*-2));
+        }
+
+        [TestMethod]
+        public void ValidTotalLevel_IsValid_ShouldWork()
+        {
+            Character character = new Character();
+            Program.Loading = false;
+
+            character.LoadCharacterSheetFromString(GetTestXml());
+            character.PlayerClass1.ClassLevel = 1;
+            character.PlayerClass2.ClassLevel = 1;
+            character.PlayerClass3.ClassLevel = 0;
+
+            Assert.IsTrue(character.ValidTotalLevel(1, 2));
+        }
+
+        [TestMethod]
+        public void ValidTotalLevel_IsNotValid_ShouldNotWork()
+        {
+            Character character = new Character();
+            //Program.Loading = false;
+
+            character.LoadCharacterSheetFromString(GetTestXml());
+            character.PlayerClass1.ClassLevel = 10;
+            character.PlayerClass2.ClassLevel = 10;
+            character.PlayerClass3.ClassLevel = 0;
+
+            Assert.IsFalse(character.ValidTotalLevel(1, 2));
+        }
+
+        [TestMethod]
         public void Copy_AllFields_ShouldWork()
         {
             Character character = new Character();
@@ -27,7 +77,64 @@ namespace MyCharacterSheetTests.Characters_Tests
 
             character.Copy(copyCharacter);
 
+            // Test Properties
             Assert.AreEqual(character.Age, copyCharacter.Age);
+            Assert.AreEqual(character.Alignment, copyCharacter.Alignment);
+            Assert.AreEqual(character.Armor, copyCharacter.Armor);
+            Assert.AreEqual(character.Background, copyCharacter.Background);
+            Assert.AreEqual(character.Bond, copyCharacter.Bond);
+            Assert.AreEqual(character.Charisma, copyCharacter.Charisma);
+            Assert.AreEqual(character.ClassResource, copyCharacter.ClassResource);
+            Assert.AreEqual(character.Constitution, copyCharacter.Constitution);
+            Assert.AreEqual(character.CP, copyCharacter.CP);
+            Assert.AreEqual(character.Dexterity, copyCharacter.Dexterity);
+            Assert.AreEqual(character.EP, copyCharacter.EP);
+            Assert.AreEqual(character.EXP, copyCharacter.EXP);
+            Assert.AreEqual(character.EyeColour, copyCharacter.EyeColour);
+            Assert.AreEqual(character.Flaw, copyCharacter.Flaw);
+            Assert.AreEqual(character.Gender, copyCharacter.Gender);
+            Assert.AreEqual(character.GP, copyCharacter.GP);
+            Assert.AreEqual(character.HairColour, copyCharacter.HairColour);
+            Assert.AreEqual(character.Height, copyCharacter.Height);
+            Assert.AreEqual(character.Ideal, copyCharacter.Ideal);
+            Assert.AreEqual(character.InitiativeBonus, copyCharacter.InitiativeBonus);
+            Assert.AreEqual(character.Intelligence, copyCharacter.Intelligence);
+            Assert.AreEqual(character.Language, copyCharacter.Language);
+            Assert.AreEqual(character.Marks, copyCharacter.Marks);
+            Assert.AreEqual(character.Movement, copyCharacter.Movement);
+            Assert.AreEqual(character.Name, copyCharacter.Name);
+            Assert.AreEqual(character.PassivePerceptionBonus, copyCharacter.PassivePerceptionBonus);
+            Assert.AreEqual(character.PersonalityBackground, copyCharacter.PersonalityBackground);
+            Assert.AreEqual(character.PersonalityNotes, copyCharacter.PersonalityNotes);
+            Assert.AreEqual(character.Pool, copyCharacter.Pool);
+            Assert.AreEqual(character.PP, copyCharacter.PP);
+            Assert.AreEqual(character.Race, copyCharacter.Race);
+            Assert.AreEqual(character.Shields, copyCharacter.Shields);
+            Assert.AreEqual(character.SkinColour, copyCharacter.SkinColour);
+            Assert.AreEqual(character.SP, copyCharacter.SP);
+            Assert.AreEqual(character.Spent, copyCharacter.Spent);
+            Assert.AreEqual(character.Strength, copyCharacter.Strength);
+            Assert.AreEqual(character.Tools, copyCharacter.Tools);
+            Assert.AreEqual(character.Trait1, copyCharacter.Trait1);
+            Assert.AreEqual(character.Trait2, copyCharacter.Trait2);
+            Assert.AreEqual(character.Vision, copyCharacter.Vision);
+            Assert.AreEqual(character.Weapons, copyCharacter.Weapons);
+            Assert.AreEqual(character.Weight, copyCharacter.Weight);
+            Assert.AreEqual(character.Wisdom, copyCharacter.Wisdom);
+
+            // Test classes
+            Assert.IsNotNull(copyCharacter.ArmorClass);
+            Assert.IsNotNull(copyCharacter.HitPoints);
+            Assert.IsNotNull(copyCharacter.Spellcasting);
+            Assert.IsNotNull(copyCharacter.Companion);
+
+            // Test Lists
+            Assert.AreEqual(character.oWeapons.Count, copyCharacter.oWeapons.Count);
+            Assert.AreEqual(character.oAmmo.Count, copyCharacter.oAmmo.Count);
+            Assert.AreEqual(character.oInventory.Count, copyCharacter.oInventory.Count);
+            Assert.AreEqual(character.oAbility.Count, copyCharacter.oAbility.Count);
+            Assert.AreEqual(character.Spellcasting.oMagic.Count, copyCharacter.Spellcasting.oMagic.Count);
+            Assert.AreEqual(character.Spellcasting.oSpells.Count, copyCharacter.Spellcasting.oSpells.Count);
         }
     }
 }
