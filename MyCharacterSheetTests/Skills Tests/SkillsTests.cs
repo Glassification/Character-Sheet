0e2c4ee19c32ca9795e4c7819a0cbeb3136394b7
Thomas Beckett <tombeckett42@live.ca>
2019-03-23 08:27:43 -0500

    Added first unit test

--- MyCharacterSheetTests/Skills Tests/SkillsTests.cs
+++ MyCharacterSheetTests/Skills Tests/SkillsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCharacterSheet.SkillsNamespace;
 
 namespace MyCharacterSheetTests.Skills_Tests
 {
@@ -9,7 +10,7 @@ namespace MyCharacterSheetTests.Skills_Tests
         [TestMethod]
         public void Bonus_AcrobaticsSpecified_ShouldWork()
         {
-            
+            Skills skill = new Skills();
         }
     }
 }
